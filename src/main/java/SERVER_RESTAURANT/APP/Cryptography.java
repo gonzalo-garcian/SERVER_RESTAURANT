7edf739c5583c9a5a71d616fb90f172d87c97f24
khugar <ksangon98@gmail.com>
2019-05-28 14:18:17 +0200

    Clase Cryptography creada. Se encarga de encriptar y desencriptar tal y como su nombre indica.

--- src/main/java/SERVER_RESTAURANT/APP/Cryptography.java
+++ src/main/java/SERVER_RESTAURANT/APP/Cryptography.java
@@ -5,12 +5,86 @@
  */
 package SERVER_RESTAURANT.APP;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.KeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
 /**
  *
  * @author Kevin
  */
 public class Cryptography {
-    
-    
-    
+
+    private Cipher rsa;
+    private PrivateKey privateKey = null;
+    private PublicKey publicKey = null;
+
+    public Cryptography() {
+        try {
+            this.privateKey = loadPrivateKey("privateKey.dat");
+            this.publicKey = loadPublicKey("publicKey.dat");
+        } catch (Exception ex) {
+            Logger.getLogger(Cryptography.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public String decryptMessage(byte[] message) throws Exception {
+
+        rsa = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        rsa.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] bytesDesencriptados = rsa.doFinal(message);
+        String textoDesencripado = new String(bytesDesencriptados);
+        System.out.println(textoDesencripado);
+        return textoDesencripado;
+
+    }
+
+    public byte[] encryptMessage(String message) throws Exception {
+        rsa = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        rsa.init(Cipher.ENCRYPT_MODE, privateKey);
+        byte[] encriptado = rsa.doFinal(message.getBytes());
+        return encriptado;
+
+    }
+
+    private static PrivateKey loadPrivateKey(String fileName) throws Exception {
+        FileInputStream fis = new FileInputStream(fileName);
+        int numBtyes = fis.available();
+        byte[] bytes = new byte[numBtyes];
+        fis.read(bytes);
+        fis.close();
+
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        KeySpec keySpec = new PKCS8EncodedKeySpec(bytes);
+        PrivateKey keyFromBytes = keyFactory.generatePrivate(keySpec);
+        return keyFromBytes;
+    }
+
+    private static PublicKey loadPublicKey(String fileName) throws Exception {
+        FileInputStream fis = new FileInputStream(fileName);
+        int numBtyes = fis.available();
+        byte[] bytes = new byte[numBtyes];
+        fis.read(bytes);
+        fis.close();
+
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        KeySpec keySpec = new X509EncodedKeySpec(bytes);
+        PublicKey keyFromBytes = keyFactory.generatePublic(keySpec);
+        return keyFromBytes;
+    }
 }
