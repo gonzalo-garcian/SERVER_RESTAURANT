f7bb165a1f5f9d2f27a507af46c0a9c2c1128b85
khugar <ksangon98@gmail.com>
2019-06-05 03:41:08 +0200

    DAO de HasDish actualizado

--- src/main/java/SERVER_RESTAURANT/DAO/HasDishDAO.java
+++ src/main/java/SERVER_RESTAURANT/DAO/HasDishDAO.java
@@ -1,5 +1,6 @@
 package SERVER_RESTAURANT.DAO;
 
+import SERVER_RESTAURANT.MODEL.Hasdish;
 import java.util.List;
 
 import org.hibernate.Query;
@@ -9,22 +10,34 @@ import SERVER_RESTAURANT.MODEL.HasdishId;
 import SERVER_RESTAURANT.UTIL.HibernateUtil;
 
 public class HasDishDAO {
+	
+	public List<Hasdish> select(){
+		List<Hasdish> hasdishList = null;
+		try {		
+			Session session = HibernateUtil.getSessionFactory().openSession();
+	        Query q = session.createQuery("From Hasdish");        
+	        hasdishList = q.list();
+	        session.close();
+		} 
+		catch (Exception e) {
+			e.printStackTrace();			
+		}
+		return hasdishList;
+	}
 
-    public List<HasdishId> selectByIdTicket(int idTicket) {
-
-        List<HasdishId> hasdishList = null;
-        HasdishId hasdishid = null;
+    public Hasdish select(int idTicket) {
+        List<Hasdish> hasdishList = null;
+        Hasdish hasdish = null;
         try {
             Session session = HibernateUtil.getSessionFactory().openSession();
-            Query q = session.createQuery("From HasDish d where d.idTicket=:idTicket");
-            q.setParameter("idTicket", idTicket);
+            Query q = session.createQuery("From Hasdish");
             hasdishList = q.list();
-            hasdishid = hasdishList.get(0);
+            hasdish = hasdishList.get(1);
             session.close();
         } catch (Exception e) {
 
         }
-        return hasdishList;
+        return hasdish;
     }
 
 }
